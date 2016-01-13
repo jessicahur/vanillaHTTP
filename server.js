8ec7f4ce17942ab4627d4767c5d841c650e59303
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-13 12:35:27 -0800

    3 working tests

---
+++ server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var url = require('url');
+
+
+var server = http.createServer(function(req, res) {
+  var path = url.parse(req.url).pathname;
+
+
+  if(path === '/time'){
+    var timeNow = new Date(Date.now()).toISOString();
+    res.write(timeNow);
+    res.end();
+  }
+
+  else if (path.substring(0, 6) === '/greet'){
+    if (req.method === 'GET'){
+      res.write('Hi there, ' + path.substring(7, path.length));
+      res.end();
+    }
+    else if (req.method === 'POST'){
+      var name = '';
+      req.on('data', function(data) {
+        body = JSON.parse(data.toString()).name;
+        res.write('Hello there '+body+'. How are you doing?');
+        res.end();
+      });
+    }
+  }
+});
+
+module.exports = server;
--- test/test.js
+++ test/test.js
@@ -1,13 +1,39 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var server = require('../server');
 var expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe('VANILLA HTTP', function() {
 
-  it('should successfully receive a response with time', function(done) {
-
+  it('should successfully receive a response with time', function() {
+    chai.request(server)
+      .get('/time')
+      .end(function(err, res){
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+      });
   });
 
+  it('should response to the GET method of /greet/paul with the correct string', function() {
+    chai.request(server)
+        .get('/greet/paul')
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res).to.be.text;
+        });
+  });
+
+  it('should response to the POST method of /greet with the correct string', function() {
+    chai.request(server)
+        .post('/greet')
+        .send({"name": "paul"})
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res).to.be.text;
+        });
+  });
 });
