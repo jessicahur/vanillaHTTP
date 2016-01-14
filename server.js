754b4bf22e77ea0664fbff52cf080f90a8f9d57c
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-14 14:38:15 -0800

    put res.writeHead to GET /time

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var server = http.createServer(function(req, res) {
 
   if(path === '/time'){
     var timeNow = new Date(Date.now()).toISOString();
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write(timeNow);
     res.end();
   }
